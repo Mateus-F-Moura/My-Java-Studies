6aa76d358584b6b6a14f53a7b6cfc482b9ee0910
Mateus-F-Moura <mf753161@gmail.com>
2025-11-15 00:56:47 -0300

    Encapsulamento: Getters, Setters, Construct e modificadores de acesso

--- src/Java_Completo/Oop/Stock_2/Product.java
+++ src/Java_Completo/Oop/Stock_2/Product.java
@@ -2,27 +2,47 @@ package Java_Completo.Oop.Stock_2;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
-    public Product () {
+    public Product() {
 
     }
 
-    public Product (String name, double price, int quantity) {
+    public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public Product (String name, double price) {
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    public double totalValueInStock() {
-        return price * quantity;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
     public void addProduct(int quantity) {
@@ -31,6 +51,10 @@ public class Product {
 
     public void removeProduct(int quantity) {
         this.quantity -= quantity;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
     }
 
     public String toString() {
