e15fb98586f1af0d595ad308771f628205b5a558
Mateus F. Moura <mf753161@gmail.com>
2025-12-07 20:50:37 -0300

    fixed logic and make a secondary diagonal as an apology

--- src/Arrays/Matrix.java
+++ src/Arrays/Matrix.java
@@ -14,7 +14,7 @@ public class Matrix {
 
             int negativeCount = 0;
             for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix.length; j++) {
+                for (int j = 0; j < matrix[i].length; j++) {
                     matrix[i][j] = sc.nextDouble();
                     if (matrix[i][j] < 0) {
                         negativeCount++;
@@ -24,9 +24,9 @@ public class Matrix {
 
             System.out.println();
             System.out.println("Overview: ");
-            for (double[] line : matrix) {
-                for (int j = 0; j < matrix.length; j++) {
-                    System.out.print(line[j] + " ");
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
                     if (j == (matrix.length - 1)) {
                         System.out.print("\n");
                     }
@@ -37,13 +37,21 @@ public class Matrix {
             System.out.println("Negative numbers count: " + negativeCount);
 
             System.out.println();
-            System.out.print("Diagonal: ");
+            System.out.print("Main Diagonal: ");
             for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix.length; j++) {
+                for (int j = 0; j < matrix[i].length; j++) {
                     if (i == j) {
                         System.out.printf("%.2f ", matrix[i][j]);
                     }
                 }
+            }
+
+            System.out.println();
+            System.out.println("Secondary Diagonal: ");
+            for (int i = 0; i < matrix.length; i++) {
+                int collum = (matrix.length - 1) - i;
+
+                System.out.printf("%.2f ", matrix[i][collum]);
             }
 
         } catch (Exception e) {
