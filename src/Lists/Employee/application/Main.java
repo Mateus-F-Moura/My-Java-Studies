7e55c4443d2c68b8a2c9c3fe17b69bf9c324e23a
Mateus F. Moura <mf753161@gmail.com>
2025-12-07 10:42:25 -0300

    add a salary increase by sector, with a id checker to not have equals id's

--- src/Lists/Employee/application/Main.java
+++ src/Lists/Employee/application/Main.java
@@ -24,14 +24,24 @@ public class Main {
                 System.out.print("ID: ");
                 int id = sc.nextInt();
 
+                boolean idExists = employees.stream().anyMatch(e -> e.getId() == id);
+                if (idExists) {
+                    System.out.println("Error: Employee with ID " + id + " already exists!");
+                    i--;
+                    continue;
+                }
+
                 System.out.print("Name: ");
                 sc.nextLine();
                 String name = sc.nextLine();
 
+                System.out.print("Sector: ");
+                String sector = sc.nextLine();
+
                 System.out.print("Salary: ");
                 double salary = sc.nextDouble();
 
-                Employee employee = new Employee(id, name, salary);
+                Employee employee = new Employee(id, name, salary, sector);
                 employees.add(employee);
             }
 
@@ -40,21 +50,49 @@ public class Main {
             showEmployees(employees);
 
             System.out.println();
-            System.out.print("Search by id: ");
-            int idSearch = sc.nextInt();
+            System.out.println("You wanna increase salary by employee or by sector? (1-Employee/2-Sector)");
+            int choose = sc.nextInt();
 
-            Employee position = employees.stream().filter(employee -> employee.getId() == idSearch).findFirst().orElse(null);
+            switch (choose) {
+                case 1:
+                    System.out.println();
+                    System.out.print("Search by id: ");
+                    int idSearch = sc.nextInt();
 
-            if (position == null) {
-                System.out.println("No id founded");
-            } else {
-                System.out.print("Percentage to increase: ");
-                int percentage = sc.nextInt();
-                position.increaseSalary(percentage);
+                    Employee position = employees.stream().filter(employee -> employee.getId() == idSearch).findFirst().orElse(null);
+
+                    if (position == null) {
+                        System.out.println("No id founded");
+                    } else {
+                        System.out.print("Percentage to increase: ");
+                        int percentage = sc.nextInt();
+                        position.increaseSalary(percentage);
+                    }
+                    break;
+                case 2:
+                    System.out.println();
+                    sc.nextLine();
+                    System.out.print("Search by sector: ");
+                    String sectorSearch = sc.nextLine();
+
+                    List<Employee> positions = employees.stream().filter(employee -> employee.getSector().equalsIgnoreCase(sectorSearch)).toList();
+
+                    System.out.print("Percentage to increase: ");
+                    int percentage = sc.nextInt();
+
+                    for (Employee employee : positions) {
+                        employee.increaseSalary(percentage);
+                    }
+                    break;
+                default:
+                    System.out.println("Not a valid answer");
+                    break;
             }
 
+
+
             System.out.println();
-            System.out.println("Employees:");
+            System.out.println("Final list of employees:");
             showEmployees(employees);
         }
     }
--- src/Lists/Employee/entities/Employee.java
+++ src/Lists/Employee/entities/Employee.java
@@ -4,12 +4,14 @@ public class Employee {
 
     private final Integer id;
     private String name;
+    private String sector;
     private Double salary;
 
-    public Employee(Integer id, String name, Double salary) {
+    public Employee(Integer id, String name, Double salary, String sector) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.sector = sector;
     }
 
     public Integer getId() {
@@ -22,6 +24,14 @@ public class Employee {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
     }
 
     public Double getSalary() {
